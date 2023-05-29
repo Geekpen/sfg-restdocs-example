b7c697b24e978eda3d01ba8a563119bfc39e33b8
Abungu Brian <brianabungu@gmail.com>
2023-05-29 17:44:56 +0300

    Document Generation

--- src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
@@ -57,7 +57,7 @@ class BeerControllerTest {
                         .param("isCold","yes")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(document("/api/v1/beer", pathParameters(
+                .andDo(document("v1/beer-get", pathParameters(
                         parameterWithName("beerId").description("UUID of desired beer to get.")
                 ),
                         requestParameters(
@@ -88,7 +88,7 @@ class BeerControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
                 .andExpect(status().isCreated())
-                .andDo(document("v1/beer",
+                .andDo(document("v1/beer-new",
                         requestFields(
                                 fields.withPath("id").ignored(),
                                 fields.withPath("version").ignored(),
